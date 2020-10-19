99af047d9be4148384a37108e95ae3c8e68b65e9
Angus Cheng <baller.industries@hotmail.com>
2020-10-19 22:23:03 +0800

    not working, dunno why

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 
 
 void puzzle1();
-void puzzle2();
+void puzzle2A();
+void puzzle2B();
 
 int main() {
     //puzzle1();
-    puzzle2();
+    puzzle2B();
 
 
     return 0;
--- puzzle2.cpp
+++ puzzle2.cpp
@@ -6,8 +6,9 @@
 //
 int min(int a, int b);
 int wrapperPaper(int length, int width, int height);
+int ribbonLength(long length, long width, long height);
 
-void puzzle2() {
+void puzzle2A() {
     FILE *file = fopen("../puzzles/2015/puzzle2.txt", "r");
 
     if (file == nullptr) {
@@ -15,16 +16,36 @@ void puzzle2() {
         exit(EXIT_FAILURE);
     }
 
-    int length;
-    int width;
-    int height;
-    int total = 0;
+    long length;
+    long width;
+    long height;
+    long total = 0;
 
-    while (fscanf(file, "%dx%dx%d", &length, &width, &height) != EOF) {
+    while (fscanf(file, "%ldx%ldx%ld", &length, &width, &height) != EOF) {
         total += wrapperPaper(length, width, height);
     }
 
-    printf("total = %d", total);
+    printf("total = %ld", total);
+}
+
+void puzzle2B() {
+    FILE *file = fopen("../puzzles/2015/puzzle2.txt", "r");
+
+    if (file == nullptr) {
+        printf("Could not open file");
+        exit(EXIT_FAILURE);
+    }
+
+    long length;
+    long width;
+    long height;
+    long total = 0;
+
+    while (fscanf(file, "%ldx%ldx%ld", &length, &width, &height) != EOF) {
+        total += ribbonLength(length, width, height);
+    }
+
+    printf("total = %ld", total);
 }
 
 int wrapperPaper(int length, int width, int height) {
@@ -36,6 +57,23 @@ int wrapperPaper(int length, int width, int height) {
     int smallestSide = min(min(sideA, sideB), min(sideA, sideC));
 
     return area + smallestSide;
+}
+
+int ribbonLength(long length, long width, long height) {
+    long volume = length * width * height;
+
+    if (length > width && length > height) {
+        // length is the largest
+        return width * 2 + height * 2 + volume;
+    }
+    else if (width > length && width > height) {
+        // width is the largest
+        return length * 2 + height * 2 + volume;
+    }
+    else {
+        // height is the largest
+        return width * 2 + length * 2 + volume;
+    }
 }
 
 int min(int a, int b) {
