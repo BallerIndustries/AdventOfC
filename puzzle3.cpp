9fba8200cafc009d07a5fb513d1c4d2e89f41b04
Angus Cheng <baller.industries@hotmail.com>
2020-10-25 11:10:36 +0800

    see ya later cmake-debug

--- puzzle3.cpp
+++ puzzle3.cpp
@@ -5,6 +5,19 @@
 //
 // Created by User on 18/10/2020.
 //
+
+struct Node {
+    int x;
+    int y;
+    Node* next;
+};
+
+Node *createNode(int x, int y);
+void listAdd(Node *head, Node *node);
+bool listContains(Node *head, int x, int y);
+
+int listSize(Node *head);
+
 int puzzle3A() {
     FILE *file = fopen("../puzzles/2015/puzzle3.txt", "r");
 
@@ -15,7 +28,10 @@ int puzzle3A() {
 
     int x = 0;
     int y = 0;
-    char c;
+    int c;
+
+    Node* head = createNode(0, 0);
+    printf("%p\n", head);
 
     do {
         c = fgetc(file);
@@ -32,14 +48,70 @@ int puzzle3A() {
         else if (c == 'v') {
             y--;
         }
+        else {
+            printf("Unexpected character c = %c", c);
+            //exit(EXIT_FAILURE);
+        }
+
+        if (!listContains(head, x, y)) {
+            //printf("Added to list");
+            Node* node = createNode(x, y);
+            listAdd(head, node);
+        }
 
         //printf("(%d, %d)\n", x, y);
     } while (c != EOF);
 
     fclose(file);
-    return 12;
+    return listSize(head);
+}
+
+int listSize(Node *head) {
+    int size = 1;
+
+    while (head->next != nullptr) {
+        size++;
+        head = head->next;
+    }
+
+    return size;
+}
+
+
+bool listContains(Node *head, int x, int y) {
+    Node* current = head;
+
+    while (current->next != nullptr) {
+        if (current->x == x && current->y == y) {
+            return true;
+        }
+
+        current = current->next;
+    }
+
+    return false;
+}
+
+Node *createNode(int x, int y) {
+    Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head->x = x;
+    head->y = y;
+    head->next = nullptr;
+
+    return head;
 }
 
 TEST_CASE( "Puzzle 3A", "[dunno]" ) {
-    REQUIRE( puzzle3A() == 1606483 );
+    // 2914 too high
+    REQUIRE( puzzle3A() == 2914 );
+}
+
+void listAdd(Node *head, Node *node) {
+    Node* current = head;
+
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+
+    current->next = node;
 }
